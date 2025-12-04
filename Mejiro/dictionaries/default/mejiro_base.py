b860182dee05572a76924fdbf7ed239d0a625f53
JEEBIS27 <umadashi27@gmail.com>
2025-12-04 21:19:10 +0900

    アスタリスク省略の動詞入力を廃止、全てのキーを押したときに出力をしないように変更、左かな＋助詞の入力を追加

--- Mejiro/dictionaries/default/mejiro_base.py
+++ Mejiro/dictionaries/default/mejiro_base.py
@@ -12,9 +12,9 @@
 
 import re
 from Mejiro.dictionaries.default.settings import (DOT, COMMA, conso_stroke_to_roma)
-from Mejiro.dictionaries.default.func import (stroke_to_kana, joshi)
-from Mejiro.dictionaries.default.abbreviations import USERS_MAP, ABSTRACT_MAP
-from Mejiro.dictionaries.default.verb import stroke_to_verb, VERB_KAMI_MAP
+from Mejiro.dictionaries.default.func import (stroke_to_kana, joshi, abstract_abbreviation_lookup)
+from Mejiro.dictionaries.default.abbreviations import USERS_MAP, VERB_KAMI_MAP
+from Mejiro.dictionaries.default.verb import stroke_to_verb
 from Mejiro.dictionaries.default.translate import kana_to_typing_output
 
 # ファイル構成
@@ -50,10 +50,10 @@ def lookup(key):
     if stroke == "n#":
         print("typing mode off")
         is_typing_mode = False
-    if stroke == "#n":
+    elif stroke == "#n":
         print("typing mode on")
         is_typing_mode = True
-    
+
     regex = re.compile(r"(S?T?K?N?)(Y?I?A?U?)(n?t?k?)(\-?#?)(S?T?K?N?)(Y?I?A?U?)(n?t?k?)(\*?)")
     regex_groups = re.search(regex, stroke)
 
@@ -65,45 +65,47 @@ def lookup(key):
     right_vowel_stroke = regex_groups.group(6)
     right_particle_stroke = regex_groups.group(7)
     asterisk = regex_groups.group(8)
-    kana_stroke = left_conso_stroke + left_vowel_stroke + '-' + right_conso_stroke + right_vowel_stroke
-    main_stroke = left_conso_stroke + left_vowel_stroke + left_particle_stroke + '-' + right_conso_stroke + right_vowel_stroke + right_particle_stroke
-
+    left_kana_stroke = left_conso_stroke + left_vowel_stroke
+    right_kana_stroke = right_conso_stroke + right_vowel_stroke
+    left_stroke = left_conso_stroke + left_vowel_stroke + left_particle_stroke
+    right_stroke = right_conso_stroke + right_vowel_stroke + right_particle_stroke
+    kana_stroke = left_kana_stroke + '-' + right_kana_stroke
+    main_stroke = left_stroke + '-' + right_stroke
+    stroke_list = [left_conso_stroke, left_vowel_stroke, left_particle_stroke, hyphen, right_conso_stroke, right_vowel_stroke, right_particle_stroke, asterisk]
     is_found_exception = [False, False] # 初期化
     result = "" # 初期化
 
     # 左右のかなを変数に格納
-    left_kana_list = stroke_to_kana(left_conso_stroke, left_vowel_stroke, left_particle_stroke, asterisk)
-    right_kana_list = stroke_to_kana(right_conso_stroke, right_vowel_stroke, right_particle_stroke, asterisk)
-    left_kana, left_extra_sound, left_conso, left_vowel = left_kana_list
-    right_kana, right_extra_sound, right_conso, right_vowel = right_kana_list
-    main_kana = left_kana + right_kana
+    left_kana_list = stroke_to_kana(stroke_list[0], stroke_list[1], stroke_list[2], stroke_list[7])
+    right_kana_list = stroke_to_kana(stroke_list[4], stroke_list[5], stroke_list[6], stroke_list[7])
+    main_kana = left_kana_list[0] + right_kana_list[0]
     # 基本形を変数に格納
-    main_base = left_kana + left_extra_sound + right_kana + right_extra_sound
+    main_base = left_kana_list[0] + left_kana_list[1] + right_kana_list[0] + right_kana_list[1]
     # 主要助詞を変数に格納
     main_joshi = joshi(left_particle_stroke, right_particle_stroke)
+    # 一般略語変換処理
+    abstract = abstract_abbreviation_lookup(left_kana_stroke, right_kana_stroke)
     # 動詞変換処理
-    verb = stroke_to_verb(kana_stroke, right_conso, right_vowel_stroke, left_particle_stroke, right_particle_stroke, left_kana, right_kana, hyphen, asterisk)
+    verb = stroke_to_verb(left_kana_list, right_kana_list, stroke_list)
 
     message = ""
     # メインの変換処理
     if main_stroke in USERS_MAP and asterisk: # ユーザー略語
         result = USERS_MAP[main_stroke]
         message = "ユーザー辞書"
-    elif kana_stroke in ABSTRACT_MAP and asterisk: # 一般略語
-        result = ABSTRACT_MAP[kana_stroke]
-        result += (main_joshi.replace("や" + COMMA, "です" + DOT).replace("や", "です"))
-        message = "一般略語"
     elif not main_kana and main_joshi and not asterisk: # 助詞
         result = main_joshi
         message = "助詞"
-    elif verb and not left_kana and right_kana and not left_extra_sound: # 動詞略語(*省略)
-        result = verb
-        message = "動詞略語"
     elif asterisk:
         # 特殊略語：～ing
-        if len(main_base) > 2 and not kana_stroke in VERB_KAMI_MAP and main_kana[-1] in ['い', 'き', 'し', 'ち', 'に', 'ひ', 'み', 'り', 'ぎ', 'じ', 'ぢ', 'び', 'ぴ', 'ぃ'] and main_base[-1] == 'ん':
+        if left_kana_list[0] and right_kana_list[0] and len(main_base) > 2 and not kana_stroke in VERB_KAMI_MAP and main_kana[-1] in ['い', 'き', 'し', 'ち', 'に', 'ひ', 'み', 'り', 'ぎ', 'じ', 'ぢ', 'び', 'ぴ', 'ぃ'] and main_base[-1] == 'ん':
             result = (main_base + 'ぐ')
             message = "特殊略語：～ing"
+        # 一般略語
+        elif abstract:
+            result = abstract
+            result += (main_joshi.replace("や" + COMMA, "です" + DOT).replace("や", "です"))
+            message = "一般略語"
         # 動詞略語
         elif verb:
             result = verb
@@ -112,8 +114,12 @@ def lookup(key):
         else:
             message = "通常出力"
             result = main_base * (2 if hyphen == "#" else 1)
+    # 左+助詞
+    elif left_kana_list[0] and not right_kana_list[0] and right_particle_stroke:
+        message = "左+助詞"
+        result = left_kana_list[0] + main_joshi
     # 通常
-    else:
+    else :
         message = "通常出力"
         result = main_base * (2 if hyphen == "#" else 1)
 
@@ -122,6 +128,9 @@ def lookup(key):
         translated_result = kana_to_typing_output(result, typing_mode)
         result = translated_result
 
+    if stroke == "STKNYIAUntk#STKNYIAUntk*":
+        message = "出力取止"
+        result = ""
     # デバッグ画面に入力と結果を表示
     print("|\t" + stroke + "\t|\t" + result + "\t|\t" + message + "\t|\t" + ("on" if is_typing_mode else "off") + "\t|")
 
